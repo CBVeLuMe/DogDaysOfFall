e4150b7bef13c622ea53d2c80da4283cd95a266f
Yi Li <plumeliiii@gmail.com>
2019-04-04 14:59:21 -0400

    Chapter 1 updated

---
+++ DogDaysOfFall/Assets/Yi/Scripts/MiniGameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class MiniGameController : MonoBehaviour
+{
+    // Chapter 1 flowchart
+    public Flowchart fcC1; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fcC1.SetBooleanVariable("FirstMiniGame", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
