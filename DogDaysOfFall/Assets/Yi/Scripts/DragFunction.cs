0a55a2710d3bf7d5454a22b431e5aad6b88ee5f8
Yi Li <plumeliiii@gmail.com>
2019-04-09 16:48:03 -0400

    Updated Chapter 2

--- DogDaysOfFall/Assets/Yi/Scripts/DragFunction.cs
+++ DogDaysOfFall/Assets/Yi/Scripts/DragFunction.cs
@@ -14,14 +14,14 @@ public class DragFunction : MonoBehaviour
 
     public bool Playerfollowing;
 
-    public GameObject youWin;
+    private EnemyController EnmCon;
 
     // Use this for initialization
     void Start()
     {
 
         Playerfollowing = false;
-
+        EnmCon = FindObjectOfType<EnemyController>();
         offset += 10;
     }
 
@@ -47,21 +47,10 @@ public class DragFunction : MonoBehaviour
         }
         if(transform.position.x == rightBlock)
         {
-            youWin.SetActive(true);
-            Time.timeScale = 0;
+            Debug.Log("You won!");
+            //EnmCon.wonGameFuc();
         }
-        //if (Input.touchCount > 0)
-        //{
-        //    Touch touch = Input.GetTouch(0); // get first touch since touch count is greater than zero
+        
 
-        //    if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved)
-        //    {
-        //        // get the touch position from the screen touch to world point
-        //        Vector3 touchedPos = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 10));
-        //        touchedPos.y = transform.position.y;
-        //        // lerp and set the position of the current object to that of the touch, but smoothly over time.
-        //        transform.position = Vector3.Lerp(transform.position, touchedPos, Time.deltaTime);
-        //    }
-        //}
     }
 }
--- DogDaysOfFall/Assets/Yi/Scripts/EnemyController.cs
+++ DogDaysOfFall/Assets/Yi/Scripts/EnemyController.cs
@@ -2,32 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 using OldMoatGames;
+using Fungus;
 
 public class EnemyController : MonoBehaviour
 {
     public GameObject BananaIdle;
     public GameObject BananaTurn;
     public GameObject youDiePic;
+    public GameObject youWinPic;
+    public GameObject winButton;
+    public GameObject loseButton;
 
+    private Vector3 startPos;
     private float Timer;
     private float randomTimer;
     private bool stopTimer = false;
     private AnimatedGifPlayer AnimatedGifPlayer;
     private DragFunction dragFuc;
-    private bool youdie = false;
     private AudioSource dieSFX;
 
     public List<float> RandomNub;
     public float RestartTime;
     public float dieTime;
 
+    public Flowchart fcC1;
+
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        randomTimer = RandomNub[Random.Range(0, RandomNub.Count)];
         dieSFX = GetComponent<AudioSource>();
         dragFuc = FindObjectOfType<DragFunction>();
+    }
+
+    private void Start()
+    {
+        randomTimer = RandomNub[Random.Range(0, RandomNub.Count)];
+        startPos = dragFuc.gameObject.transform.position;
+        Timer = 0f;
     }
 
     // Update is called once per frame
@@ -46,25 +58,46 @@ public class EnemyController : MonoBehaviour
             Invoke("CheckPlayer",dieTime);
             Invoke("Restart", RestartTime);
         }
-        if (youdie)
-        {
-            if (dragFuc.Playerfollowing)
-            {
-                youDiePic.SetActive(true);
-                Time.timeScale = 0;            
-            }
-        }
 
     }
+    void lostGameCheck()
+    {
+        if (dragFuc.Playerfollowing)
+        {
+            youDiePic.SetActive(true);
+            loseButton.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+    public void ResetGameButton()
+    {
+        dragFuc.gameObject.transform.position = startPos;
+        Restart();
+        youDiePic.SetActive(false);
+        loseButton.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void wonGameFuc()
+    {
+        winButton.SetActive(true);
+        youWinPic.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void wonButton()
+    {
+        Time.timeScale = 1;
+        fcC1.SetBooleanVariable("StealthGameWon", true);
+    }
+
     void CheckPlayer()
     {
-        youdie = true;
+        lostGameCheck();
         dieSFX.Play();
     }
     void Restart()
     {
         stopTimer = false;
-        youdie = false;
         randomTimer = RandomNub[Random.Range(0, RandomNub.Count)];
         BananaIdle.SetActive(true);
         BananaTurn.SetActive(false);
