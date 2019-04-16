b6948e6d3e2e184aad28e2933214a357ae332a4c
Changbai Guo <33477119+CBVeLuMe@users.noreply.github.com>
2019-04-16 11:44:47 -0400

    add three testing scripts

---
+++ DogDaysOfFall/Assets/Shijun/Scripts/ShadowingRenderer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShadowingRenderer : MonoBehaviour
+{
+    public float maxTime;
+    public float time;
+    public Image timeBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeBar.fillAmount = time / maxTime;
+    }
+}
