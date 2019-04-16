776c9ff9ce1e374854092f25a9f7da9dc21211c0
Changbai Guo <33477119+CBVeLuMe@users.noreply.github.com>
2019-04-16 13:01:08 -0400

    Fix the saving

---
+++ DogDaysOfFall/Assets/Shijun/Scripts/AlternativeSaveMenu.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class API03GameObject : MonoBehaviour
+{
+    private void Start()
+    {
+        GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        Debug.Log(go.activeInHierarchy);
+        //设置为未激活状态。
+        go.SetActive(false);
+        Debug.Log(go.activeInHierarchy);
+        Debug.Log(go.tag);
+    }
+}
