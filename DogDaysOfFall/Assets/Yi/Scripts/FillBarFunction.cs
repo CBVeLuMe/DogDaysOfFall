5523f34fb7216d3d309f22347fa2ca485558a966
Lin Qi <linqi7070@gmail.com>
2019-05-09 13:11:18 -0400

    Chapter 2 save points

--- DogDaysOfFall/Assets/Yi/Scripts/FillBarFunction.cs
+++ DogDaysOfFall/Assets/Yi/Scripts/FillBarFunction.cs
@@ -8,29 +8,40 @@ public class FillBarFunction : MonoBehaviour
     [SerializeField] float GreenSpeed;
     [SerializeField] float GreenDistance;
     [SerializeField] GameObject rayCastCenter;
-
+    private bool startFill;
     private void Start()
     {
         StopOrNot = false;
+        Invoke("FillActivate",1f);
     }
+
+    void FillActivate()
+    {
+        startFill = true;
+    }
+
     private void Update()
     {
-        LayerMask handle = LayerMask.GetMask("StealthHandle");
-        RaycastHit2D hit = Physics2D.Raycast(rayCastCenter.transform.position, Vector2.right, GreenDistance, handle);
-
-        if (hit.collider)
+        if (startFill)
         {
-            StopOrNot = true;
-        }
-        else if (!hit.collider)
-            StopOrNot = false;
+            LayerMask handle = LayerMask.GetMask("StealthHandle");
+            RaycastHit2D hit = Physics2D.Raycast(rayCastCenter.transform.position, Vector2.right, GreenDistance, handle);
 
-        if (!StopOrNot)
-        {
-            transform.localScale += new Vector3(Time.deltaTime * GreenSpeed, 0, 0);
+            if (hit.collider)
+            {
+                StopOrNot = true;
+            }
+            else if (!hit.collider)
+                StopOrNot = false;
+
+            if (!StopOrNot)
+            {
+                transform.localScale += new Vector3(Time.deltaTime * GreenSpeed, 0, 0);
+            }
+            else
+                return;
         }
-        else
-            return;
+       
     }
 
     public void ResetGreen()
