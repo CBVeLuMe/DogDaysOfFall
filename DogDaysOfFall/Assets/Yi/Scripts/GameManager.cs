d74f7e20ff0ed021e866710b1311b81cff35b7e7
Yi Li <plumeliiii@gmail.com>
2019-05-14 17:16:51 -0400

    Updated Main Menu

---
+++ DogDaysOfFall/Assets/Yi/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Fungus;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager _instance;
+
+    private float WSpeed;
+    private GameObject Fwriter;
+
+    void Awake()
+    {
+        if (_instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        _instance = this;
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (Fwriter = GameObject.FindGameObjectWithTag("SayDialogue"))
+        {
+            Fwriter.GetComponent<Writer>().WritingSpeed = WSpeed;
+        }
+        else
+            return;
+    }
+}
