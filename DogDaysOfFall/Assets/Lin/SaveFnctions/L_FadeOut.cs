21e4a6195b82b66ce8371f8ed9a3e33ef8070466
Lin Qi <linqi7070@gmail.com>
2019-06-19 16:34:10 -0400

    Screen fade

---
+++ DogDaysOfFall/Assets/Lin/SaveFnctions/L_FadeOut.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class L_FadeOut : MonoBehaviour
+{
+    private GameObject uiCanvas;
+    public Animator fadeAnimator;
+    void OnEnable()
+    {
+        uiCanvas = this.transform.parent.gameObject;
+        fadeAnimator.SetBool("PlayAnimation",true);
+    }
+    public void FadeAnimationOut()
+    {
+        fadeAnimator.SetBool("PlayAnimation", false);
+        uiCanvas.SetActive(false);
+        
+    }
+}
