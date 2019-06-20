eff859c14d3fe5c27807509f3678f75d6e2ad48a
Yi Li <plumeliiii@gmail.com>
2019-06-20 17:08:26 -0400

    Updated scrolling background

---
+++ DogDaysOfFall/Assets/Yi/Scripts/ScrollingBG.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollingBG : MonoBehaviour
+{
+    [SerializeField] RawImage[] bgs;
+
+    [SerializeField] float speed;
+
+    float finalspeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        finalspeed += speed * Time.deltaTime;
+
+        //bgs[0].uvRect = new Rect(speed * Time.deltaTime, speed *Time.deltaTime, 1f, 1f);
+        for (int i = 0; i < bgs.Length; i++)
+        {
+            bgs[i].uvRect = new Rect(finalspeed, finalspeed, 1f, 1f);
+        }
+    }
+}
