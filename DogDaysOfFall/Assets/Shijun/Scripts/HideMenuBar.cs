7dcbe1eb1f98f14775fca46c5175966d48279d07
Changbai Guo <33477119+CBVeLuMe@users.noreply.github.com>
2019-06-26 18:04:12 -0400

    Hide Menu

---
+++ DogDaysOfFall/Assets/Shijun/Scripts/HideMenuBar.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class HideMenuBar : MonoBehaviour
+{
+    private GameObject menuButton;
+
+    private void Start()
+    {
+        menuButton = GameObject.FindGameObjectWithTag("MenuButton");
+
+    }
+
+    public void SwitchMenuBar()
+    {
+        if (menuButton != null)
+        {
+            if (menuButton.activeSelf)
+            {
+                menuButton.SetActive(false);
+            }
+            else
+            {
+                menuButton.SetActive(true);
+            }
+        }
+    }
+}
