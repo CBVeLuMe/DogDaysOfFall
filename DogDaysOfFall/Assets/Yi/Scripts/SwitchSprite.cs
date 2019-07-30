7caff820ae8f9fc16525d76711afd401cfd6ad96
Yi Li <plumeliiii@gmail.com>
2019-07-30 18:57:14 -0400

    FINALLY ADDED ALL THE ARTS

--- DogDaysOfFall/Assets/Yi/Scripts/SwitchSprite.cs
+++ DogDaysOfFall/Assets/Yi/Scripts/SwitchSprite.cs
@@ -8,14 +8,32 @@ public class SwitchSprite : MonoBehaviour
     public Sprite foodOneFlash;
     public Sprite foodTwoFlash;
 
+    private Sprite homeback1;
+    private Sprite foodback1;
+    private Sprite foodback2;
+    private Sprite foodback3;
+
     public SpriteRenderer groceryOne;
     public SpriteRenderer groceryTwo;
     public SpriteRenderer groceryThree;
     public void switchSprite()
     {
+        homeback1 = GetComponent<SpriteRenderer>().sprite;
+        foodback1 = groceryOne.sprite;
+        foodback2 = groceryTwo.sprite;
+        foodback3 = groceryThree.sprite;
+
         GetComponent<SpriteRenderer>().sprite = switchedSprite;
         groceryOne.sprite = foodOneFlash;
         groceryTwo.sprite = foodTwoFlash;
         groceryThree.sprite = foodTwoFlash;
     }
+    public void switchSpriteBack()
+    {
+        GetComponent<SpriteRenderer>().sprite = homeback1;
+        groceryOne.sprite = foodback1;
+        groceryTwo.sprite = foodback2;
+        groceryThree.sprite = foodback3;
+    }
+
 }
