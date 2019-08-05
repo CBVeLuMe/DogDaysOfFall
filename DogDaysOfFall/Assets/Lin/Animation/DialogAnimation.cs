17fd8c53369aa0df1319f72e3be32fcf89ee71fb
Lin Qi <linqi7070@gmail.com>
2019-08-05 14:35:07 -0400

    Main Menu animation

---
+++ DogDaysOfFall/Assets/Lin/Animation/DialogAnimation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogAnimation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject continueButton;
+    public Animator dialogAnimation;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnEnable()
+    {
+        dialogAnimation.SetBool("PlayDialog",true);
+    }
+    public void EndAnimation()
+    {
+        continueButton.SetActive(true);
+        dialogAnimation.enabled = false;
+    }
+
+    public void ClickDialog()
+    {
+        dialogAnimation.enabled = false;
+        continueButton.SetActive(true);
+    }
+}
---
+++ DogDaysOfFall/Assets/Lin/Animation/FadeCanvas.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeCanvas : MonoBehaviour
+{
+    public GameObject beforeChapter;
+
+    private Animator fadeAnimator;
+
+
+    void OnEnable()
+    {
+        fadeAnimator = this.gameObject.GetComponent<Animator>();
+        fadeAnimator.SetBool("PlayFade",true);
+    }
+    public void FadeCanvasComplete()
+    {
+        beforeChapter.SetActive(true);
+        this.gameObject.transform.parent.gameObject.SetActive(false);
+    }
+}
